7d09697d5c6eb4add4d448f25959215dfe541694
marinaeleshaa <marinaeleshah77@gmail.com>
2025-12-04 17:20:10 +0200

    feat: add iframe for studio location

--- src/components/features/contact/ContactLayout.tsx
+++ src/components/features/contact/ContactLayout.tsx
@@ -6,10 +6,10 @@ import ContactLocation from "./ContactLocation";
 const contactLayout = () => {
   return (
     <div className="max-w-[70%] mx-auto">
-      <div className="grid gap-15 grid-cols-1 grid-rows-auto md:grid-rows-2 md:grid-cols-2">
+      <div className="grid gap-15 grid-cols-1  md:grid-cols-2">
         <ContactForm className="w-full "/>
         <ContactDetails className="w-full " />
-        <ContactLocation className="col-span-1 md:col-span-2 bg-amber-800" />
+        <ContactLocation className="col-span-1 md:col-span-2 h-[40vh] bg-amber-800" />
       </div>
     </div>
   );
