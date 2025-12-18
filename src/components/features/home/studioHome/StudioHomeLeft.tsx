33f79cc9269edf56e05435743074af974ea37b02
marinaeleshaa <marinaeleshah77@gmail.com>
2025-12-19 00:24:48 +0200

    style: change img in gallery section at home

--- src/components/features/home/studioHome/StudioHomeLeft.tsx
+++ src/components/features/home/studioHome/StudioHomeLeft.tsx
@@ -6,15 +6,15 @@ const GalleryHomeLeft = ({ className }: { className?: string }) => {
     <div className={`${className}  `}>
       <div className="w-full h-[300px] md:h-[500px] sm:h-[500px] relative  ">
         <Image
-          src="/home/about1.jpg"
+          src="/home/2.jpg"
           alt="about"
           width={900}
           height={900}
           className="object-cover rounded-lg w-full h-full "
         />
-        <div className="absolute  w-[50%] h-[70%] -bottom-[10%] left-0 -translate-x-1/3 ">
+        <div className="absolute   w-[50%] h-[70%] -bottom-[10%] left-0 -translate-x-1/3 ">
           <Image
-            src="/home/about2.jpg"
+            src="/home/about3.jpg"
             alt="about"
             width={900}
             height={900}
--- src/components/features/home/studioHome/StudioHomeRight.tsx
+++ src/components/features/home/studioHome/StudioHomeRight.tsx
@@ -1,11 +1,17 @@
 import MyBtn from "@/components/ui/MyBtn";
-import { useTranslations } from "next-intl";
+import { getTranslations } from "next-intl/server";
+import { cookies } from "next/headers";
 
-const GalleryHomeRight = ({ className }: { className?: string }) => {
-  const t = useTranslations("home.homeStudio");
+const GalleryHomeRight =async ({ className }: { className?: string }) => {
+  const cookieStore = await cookies();
+  const lang = cookieStore.get("NEXT_LOCALE")?.value || "en";
+  const t = await getTranslations({
+    locale: lang,
+    namespace: "home.homeStudio",
+  })
   const points = t.raw("points");
   return (
-    <div className={`${className} p-4 group space-y-5`}>
+    <div dir={lang === "ar" ? "rtl" : "ltr"} className={`${className} p-4 group space-y-5`}>
       <p className="text-primary/80 relative w-fit capitalize font-bold text-md md:text-lg  animated-underline">
         {t("sectionLabel")}
       </p>
