f2e0d7927aa1b507009b27dfc590d1903aecaec7
marinaeleshaa <marinaeleshah77@gmail.com>
2026-01-15 23:59:24 +0200

    style: change the site images to be regarding to the charity

--- src/components/shared/whyChooseUs/WhyChooseUsLeft.tsx
+++ src/components/shared/whyChooseUs/WhyChooseUsLeft.tsx
@@ -4,7 +4,7 @@ const WhyChooseUsLeft = ({className}:{className:string}) => {
   return (
    <div className={`${className}   rounded-lg`}>
         <div className='h-full'>
-            <Image src="/home/1.jpg" alt="about" width={900} height={900} className="object-cover rounded-lg w-full h-full "/>
+            <Image src="/home/three.jpg" alt="about" width={900} height={900} className="object-cover rounded-lg w-full h-full "/>
         </div>
     </div>
   )
