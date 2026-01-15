73b85fca46252e3998988a305101a0c6a9bae117
marinaeleshaa <marinaeleshah77@gmail.com>
2026-01-16 01:50:06 +0200

    fix: home service card appearance

--- src/components/features/home/servicesHome/ServiceCard.tsx
+++ src/components/features/home/servicesHome/ServiceCard.tsx
@@ -9,7 +9,7 @@ const ServiceCard = ({ service }: { service: IService }) => {
         className="
           bg-foreground/70 rounded-lg flex flex-col justify-center 
           items-center text-primary-foreground hover:scale-105 transition 
-          duration-300 h-[300px] overflow-hidden relative
+          duration-300 h-75 overflow-hidden relative
         "
       >
         <Image
@@ -25,32 +25,35 @@ const ServiceCard = ({ service }: { service: IService }) => {
           className="
             absolute bottom-0 left-0 w-full
             bg-dark/80 
-            flex flex-col items-center justify-evenly
+            flex flex-col items-center
             overflow-hidden p-4 
-            h-[60px] sm:h-[65px] md:h-[70px] lg:h-[60px] group-hover:h-60 sm:group-hover:h-[190px] md:group-hover:h-[250px]
-           lg:group-hover:h-[220px] transition-[height] duration-500 ease-out
+            transition-all duration-500 ease-out
           "
         >
           {/* Title: always visible */}
-          <p className="text-dark-foreground text-lg font-bold ">
+          <p className="text-dark-foreground text-center text-lg font-bold shrink-0">
             {service.title}
           </p>
 
           {/* Hidden content: appears on hover */}
           <div
             className="
-              opacity-0 translate-y-4
+              grid grid-rows-[0fr] 
               transition-all duration-500 ease-out
-              group-hover:opacity-100 group-hover:translate-y-0
-              text-center px-4 mt-2 flex flex-col gap-2 
+              group-hover:grid-rows-[1fr]
+              w-full
             "
           >
-            <p className="line-clamp-3">{service.description}</p>
-            <MyBtn
-              text={service.action.text}
-              href={service.action.href}
-              width="full"
-            />
+            <div className="overflow-hidden">
+              <div className="text-center px-4 mt-2 flex flex-col gap-2">
+                <p className="line-clamp-3">{service.description}</p>
+                <MyBtn
+                  text={service.action.text}
+                  href={service.action.href}
+                  width="full"
+                />
+              </div>
+            </div>
           </div>
         </div>
       </div>
