cb1243d15515f1f800daf5ef21337d22d6c8b975
marinaeleshaa <marinaeleshah77@gmail.com>
2026-01-16 03:11:57 +0200

    style: enhance contact form layout

--- src/components/features/contact/ContactLayout.tsx
+++ src/components/features/contact/ContactLayout.tsx
@@ -6,10 +6,13 @@ import ContactLocation from "./ContactLocation";
 const contactLayout = () => {
   return (
     <div className="w-myWidth mx-auto">
-      <div className="grid gap-15 grid-cols-1  md:grid-cols-2 lg:grid-cols-3">
-        <ContactForm className="w-full col-span-1  lg:col-span-2"/>
-        <ContactDetails className="w-full " />
-        <ContactLocation className="col-span-1 md:col-span-2 lg:col-span-3 h-[40vh]" />
+      <div className="flex flex-col gap-15  ">
+        <div className="flex flex-col md:flex-row gap-15">
+
+        <ContactForm className="w-full md:w-2/3 "/>
+        <ContactDetails className="w-full md:w-1/3  " />
+        </div>
+        <ContactLocation className=" h-[40vh]" />
       </div>
     </div>
   );
