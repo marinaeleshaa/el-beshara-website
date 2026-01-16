4ffd98344d0c1683329fab924a696bc8531e097e
marinaeleshaa <marinaeleshah77@gmail.com>
2026-01-16 02:37:36 +0200

    refactor: remove unused data files

--- src/data/NavMenuItems.tsx
+++
@@ -1,7 +0,0 @@
-export const NavMenuItems = [
-  { title: "Home", url: "/" },
-  { title: "About Us", url: "/about" },
-  { title: "Services", url: "/services" },
-  { title: "Gallery", url: "/gallery/images" },
-  { title: "Contact Us", url: "/contact" },
-];
