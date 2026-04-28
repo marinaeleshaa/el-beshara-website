6a5e57c23d129e23d24e1a9acfd03f41a0e78fe9
marinaeleshaa <marinaeleshah77@gmail.com>
2026-04-28 16:27:00 +0300

    fix navbar crash when API fails or server is unavailable

--- src/components/shared/Navbar.tsx
+++ src/components/shared/Navbar.tsx
@@ -4,6 +4,7 @@ import { getAbout } from "@/lib/api/about";
 import { getTranslations } from "next-intl/server";
 import NavbarClient from "./NavbarClient";
 import { IsProfile } from "@/lib/guards/IsProfile";
+import { IProfile } from "@/lib/Interfaces/AboutInterface";
 
 interface NavbarProps {
   className?: string;
@@ -14,10 +15,25 @@ const Navbar = async ({ className }: NavbarProps) => {
   const t = await getTranslations("common");
 
   const menu = t.raw("menu");
-  if(!IsProfile(profile)) return null;
+
+  // ! just till the api is ready
+  const safeProfile = IsProfile(profile)
+  ? profile
+  : ({
+      logo: "/nav-logo.png",
+      socialMedia: [],
+      email: "",
+      phoneNumbers: [],
+      address: {
+        building: "",
+        street: "",
+        city: "",
+      },
+    } as unknown as IProfile);
+  // if(!IsProfile(profile)) return null;
   return (
     <NavbarClient
-      profile={profile}
+      profile={safeProfile}
       menu={menu}
       studioName={t("studioName")}
       className={className}
